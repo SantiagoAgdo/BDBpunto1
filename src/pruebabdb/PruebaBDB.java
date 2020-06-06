bae9e62d1875ed1daaf3352db759d87f53087ed7
Santiago Agudelo R <sagudelo@hightech.com.co>
2020-06-06 15:42:29 -0500

    Se agrega metodo

--- src/pruebabdb/PruebaBDB.java
+++ src/pruebabdb/PruebaBDB.java
@@ -17,7 +17,9 @@ public class PruebaBDB {
     public static void main(String[] args) {
         revision rv = new revision();
         caracteristica2 c2 = new caracteristica2();
+        int hora = 10;
         System.out.println("estado: " + c2.state());
+        System.out.println("esta abierto? " + (c.abierto(hora) ? "Si":"No"));
     }
     
 }
